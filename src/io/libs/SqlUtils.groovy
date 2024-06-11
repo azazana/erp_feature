f35c9db0593c634039cbfc6031657cdb1f2a4075
Борзенкова Анна Викторовна <borzenkova@kami.local>
2024-06-11 07:26:02 +0300

    test: restoreDB

--- src/io/libs/SqlUtils.groovy
+++ src/io/libs/SqlUtils.groovy
@@ -155,7 +155,7 @@ def restoreDb(dbServer, infobase, backupDir, sqlUser, sqlPwd) {
     //     sqlPwdPath = "-P ${sqlPwd}"
     // }
 
-    // def latestBackup = getLatestBackup(backupDir)
+    def latestBackup = getLatestBackup(backupDir)
     // def command = "sqlcmd -S localhost -U sa -P bVeqxLh7btw87z7d -i \"C:\\Users\\Support1c\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\erp_features\\copy_etalon\\restore.sql\" -b -v restoreddb=test_erp_test -v bakfile=\"\\\\rs-backup\\erp_backup\\erp_w_001\\erp_w_001_backup_2023_12_25_230001_0904933.bak\""
     // def command = "sqlcmd -S localhost -U sa -P bVeqxLh7btw87z7d -i \"C:\\Users\\Support1c\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\erp_features\\copy_etalon\\restore.sql\" -b -v restoreddb=test_erp_test -v bakfile=\"\\\\rs-backup\\erp_backup\\erp_w_001\\erp_w_001_backup_2023_12_25_230001_0904933.bak\""
     // echo "Executing command work: ${command}"
