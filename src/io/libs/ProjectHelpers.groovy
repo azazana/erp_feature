bb807c546b7d2e42f79a60217333596303fac416
Борзенкова Анна Викторовна <borzenkova@kami.local>
2024-06-18 13:54:42 +0300

    block sessions

--- src/io/libs/ProjectHelpers.groovy
+++ src/io/libs/ProjectHelpers.groovy
@@ -237,7 +237,7 @@ def bindExtRepo(platform1c, server1c, testbase, admin1cUser, admin1cPwd, storage
 def killDesinerSession(platform1c, server1c, testbase, admin1cUser, admin1cPwd, unlock_code = "") {
     utils = new Utils()
     
-    cmd_line = "vrunner session kill --filter appid=Designer --ras ${server1c}:1545 --rac C:\\Program Files\\1cv8\\${platform1c}\\bin --db ${testbase}"
+    cmd_line = "vrunner session kill --filter appid=Designer --ras ${server1c}:1545 --rac C:\\Program Files\\1cv8\\${platform1c}\\bin --db ${testbase} --db-user ${admin1cUser} --db-pwd ${admin1cPwd}"
     echo cmd_line
     returnCode = utils.cmd(cmd_line)
      if (unlock_code != "") {
